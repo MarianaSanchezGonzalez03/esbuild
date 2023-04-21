fa91cb4842c813ce12dca0d0473c5111a8850913
MarianaSanchezGonzalez03 <mari98san33@gmail.com>
2023-04-21 00:40:09 -0600

    Empaquetando un proyecto

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(
-    <App message= "Platzi ESBuild Example "/>,
-    document.getElementById("app")
-;)
+   <App message = "Platzi EsBuild Example"/>,
+   document.getElementById("app")
+);
